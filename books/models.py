e8c048f8a3afd36ac268dc8bfae855f126cdc5c9
Michael L Hart <mlhart58@gmail.com>
2016-03-13 19:21:36 -0400

    Register Book in admin and improve Book model

--- books/models.py
+++ books/models.py
@@ -5,11 +5,17 @@ from django.db import models
 class Book(models.Model):
 	""" Class doc """
 	title	= models.CharField(max_length=150)
-	author	= models.CharField(max_length=70)
+	author	= models.CharField(
+		max_length=70,
+		help_text="Use pen name, not real name"
+	)
 	review	=	models.TextField(blank=True, null=True)
 	date_reviewed	=	models.DateTimeField(blank=True, null=True)
-	is_favourite	= models.BooleanField(default=False)
+	is_favourite	= models.BooleanField(
+		default=False,
+		verbose_name="Favourite?"
+	)
+
 	
-	def __init__ (self):
-		""" Class initialiser """
-		pass
+	def __str__	(self):
+		return self.title
