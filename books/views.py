516a773db47a0988b42fe6b6ca9412e753d00eef
Michael L Hart <mlhart58@gmail.com>
2016-03-13 22:27:58 -0400

    Create our first URL and view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from	django.http				import	HttpResponse
+from	django.shortcuts	import	render
 
 # Create your views here.
+def list_books (request):
+	""" Function doc """
+	return HttpResponse(request.user.username)
+	
