495887529136171bf8611676bfa11a62ce0e0782
Michael L Hart <mlhart58@gmail.com>
2016-03-19 23:03:03 -0400

    Test list_books view

---
+++ books/tests/test_views.py
@@ -0,0 +1,31 @@
+from	django.test								import	TestCase
+from	django.core.urlresolvers	import	resolve, reverse
+from	books.models							import	Book
+from	books.views								import	list_books, ReviewList
+from	books.factories						import (AuthorFactory, BookFactory,
+																				ReviewFactory, UserFactory)
+
+
+class TestListBooks(TestCase):
+	"""
+	"""
+	def test_list_books_url(self):
+		url	=	resolve('/')
+		self.assertEqual(url.func, list_books)
+		
+	def test_list_books_template(self):
+		response = self.client.get(reverse(list_books))
+		self.assertTemplateUsed(response, 'list.html')
+		
+	def test_list_books_returns_books_with_reviews(self):
+		#setup out data
+		author								= AuthorFactory()
+		book_with_reviews			= ReviewFactory.create_batch(2, authors=[author,])
+		book_without_reviews	=	BookFactory.create_batch(4, authors=[author,])
+		
+		response	= self.client.get(reverse(list_books))
+		books			=	list(response.context['books'])
+		
+		self.assertEqual(books_with_reviews, books)
+		self.assertNotEqual(books_without_reviews, books)
+		
